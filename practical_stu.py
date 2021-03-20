af9412d7d34f9d1a3e09e7cd6ea82eefd18b5150
Austin Rhode <ajr3021@g.rit.edu>
2021-03-20 18:59:40 -0400

    Added practical files

---
+++ practical_stu.py
@@ -0,0 +1,49 @@
+import arrays
+import random
+
+"""
+File Reading
+
+You have been given a file called "pokemon.csv". Your job is to use the file to print the name of the Pokemon of the given type that has the highest stat of the given stat type.
+For example, find_highest_stat("Fire", "HP") should print "The Fire pokemon with the most HP is Emboar".
+"""
+def find_highest_stat(a_type, stat):
+    return None
+
+"""
+Arrays
+
+Write a function that, given an array, creates another array called "squares" that holds the values of the original array, squared.
+Then return the equivalent of the original array concatenated with squares. (Hint: you will need to create another array to do this!)
+"""
+def square_elements(an_array):
+    return None
+
+"""
+Lists
+
+Write a function that, given a filename and a letter, prints out the elements that start with the given letter. You must use a list at some point in your function!
+If no element is found, print "No element starts with the letter <letter>"
+"""
+def find_elements(filename, letter):
+    return None
+
+    
+"""
+Write a function that returns true if the list is a palindrome and
+false if the list is not a palidrome.
+
+Do this without using slicing
+"""
+def is_palidrome(a_list):
+    return False
+
+
+"""
+Recursion
+
+Write a function that recursively computes the gcd of two numbers given as parameters. Then, write at least one test function to test your gcd function.
+"""
+def gcd(num1, num2):
+    return None
+
